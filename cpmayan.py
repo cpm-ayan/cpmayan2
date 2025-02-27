76711eb4c87ff854269c3350df5a87d58aa3c709
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-27 05:37:02 +0500

    Update cpmayan.py

--- cpmayan.py
+++ cpmayan.py
@@ -12,7 +12,7 @@ import requests
 
 BASE_URL: str = "https://cpmayan2.squareweb.app/api"
 
-class CPMAyan:
+class CPMAyan2:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None

e2dbef341add7d4f1376defb8536e6ffcd1f525d
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-27 05:38:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
-from cpmayan2 import CPMAyan2
+from cpmayan import CPMAyan
 
 __CHANNEL_USERNAME__ = "CPMNuker"
 __GROUP_USERNAME__   = "CPMNukerChat"
@@ -128,7 +128,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMAyan2(acc_access_key)
+        cpm = CPMAyan(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:

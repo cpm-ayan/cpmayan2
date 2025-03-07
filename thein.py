8509ff0ea8671fc6951868c182f86bd9258a605e
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-07 10:09:21 +0500

    Update and rename cpmayanshopp.py to thein.py

--- thein.py
+++ thein.py
@@ -12,7 +12,7 @@ import requests
 
 BASE_URL: str = "https://cpmayanshopp.squareweb.app/api"
 
-class CPMAyanShopp:
+class Thein:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None

7e06adb94ac7e4960ec8b3cfeea76bfd8fed27f7
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-03-08 11:24:16 +0500

    Update and rename cpmtooldevz.py to ayan.py

--- ayan.py
+++ ayan.py
@@ -2,7 +2,7 @@ import requests
 
 __ENDPOINT_URL__: str = "https://ayan/squareweb.app/api"
 
-class CPMTooldevz:
+class Ayan:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
